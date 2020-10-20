031532e91ec47fb0fbeb2eabbd0d88fc0e74665f
Egor <lalalayes2@gmail.com>
2020-10-20 21:03:11 +0300

    feat: solved brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  while (str.length !== 0 && bracketsConfig.reduce(function (flag, item) {flag = flag || str.includes(item.join('')); return flag;}, flag = false)){
+    for(let i = 0; i < bracketsConfig.length; i++)
+    {
+        while (str.includes(bracketsConfig[i].join(''))){
+            str = str.replace(bracketsConfig[i].join(''), '')
+        }
+    }
+}
+return str.length === 0 ? true : false;
 }
